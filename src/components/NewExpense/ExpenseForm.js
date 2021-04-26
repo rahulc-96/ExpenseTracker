7387d004b3a12b983211b323ef41f9efc34b35f7
ZEUS <3crahulrao@gmail.com>
2021-04-26 23:59:33 +0530

    conditional rendering of new expenses form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,6 +6,8 @@ const ExpenseForm = (props) => {
   const [enteredAmount, setExpenseAmount] = useState("");
   const [enteredDate, setExpenseDate] = useState("");
 
+  const [isFormRendered, setIsFormRendered] = useState(false);
+
   const titleChangeHandler = (event) => {
     setExpenseTitle(event.target.value);
   };
@@ -23,16 +25,40 @@ const ExpenseForm = (props) => {
     const enteredExpense = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: new Date (enteredDate),
+      date: new Date(enteredDate),
     };
     props.onSaveEnteredExpense(enteredExpense);
-    setExpenseTitle('');
-    setExpenseDate('');
-    setExpenseAmount('');
+    setIsFormRendered(false);
+    setExpenseTitle("");
+    setExpenseDate("");
+    setExpenseAmount("");
   };
 
+  const resetHandler = (event) => {
+    event.preventDefault();
+    setExpenseTitle("");
+    setExpenseDate("");
+    setExpenseAmount("");
+    setIsFormRendered(false);
+  };
+
+  const addExpenseHandler = (event) => {
+    event.preventDefault();
+    setIsFormRendered(true);
+  };
+
+  if (!isFormRendered) {
+    return (
+      <div className="new-expense__controller">
+        <button type="button" className="new-expense__controller__button" onClick={addExpenseHandler}>
+          Add Expenses
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <form onSubmit={saveEnteredExpense}>
+    <form onSubmit={saveEnteredExpense} onReset={resetHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -63,6 +89,7 @@ const ExpenseForm = (props) => {
           />
         </div>
         <div className="new-expense__actions">
+          <button type="reset"> Cancel </button>
           <button type="submit"> Add Expense </button>
         </div>
       </div>
